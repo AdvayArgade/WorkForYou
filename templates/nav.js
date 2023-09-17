b754229a3d3410e21525117d88d4db0f64b77930
Asit9607 <142369272+Asit9607@users.noreply.github.com>
2023-09-17 15:53:42 +0530

    Update nav.js

--- templates/nav.js
+++ templates/nav.js
@@ -1,1 +1,12 @@
+function navigationLoader(){
+    fetch('nav.html')
+    .then(response => response.text())
+        .then(data => {
+            document.getElementById('nav').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading navigation:', error);
+        });
+}
+document.addEventListener('DOMContentLoaded',navigationLoader);
 
